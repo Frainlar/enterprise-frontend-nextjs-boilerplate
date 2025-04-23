742ee3f1f6a58c15b00732328f18a980ea6075a0
Edson Frainlar <fedson@kaartech.com>
2025-04-23 20:50:20 +0000

    KKK

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./apps/**/*.{js,ts,jsx,tsx}",
+    "./libs/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
